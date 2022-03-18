3089a2361336d255f436066cc7104a3a63c77950
Poós Dániel <poosdani@gmail.com>
2022-03-18 11:08:57 +0100

    added two classes

---
+++ app/src/main/java/com/example/slab_warriors/data/Game.java
@@ -0,0 +1,15 @@
+package com.example.slab_warriors.data;
+
+public class Game {
+    private String id;
+    private int userId, fighterId;
+
+    public Game(String id, int userId, int fighterId) {
+        this.id = id;
+        this.userId = userId;
+        this.fighterId = fighterId;
+    }
+    public Game(String id, int userId, int fighterId, Deck deck){
+        //
+    }
+}
---
+++ app/src/main/java/com/example/slab_warriors/data/Match.java
@@ -0,0 +1,51 @@
+package com.example.slab_warriors.data;
+
+public class Match {
+    private User player;
+    private Game game;
+    private Fighter fighter;
+    private Enemy enemy;
+    private Deck deck;
+    private final int placeableCards = 3;
+
+    public Match(User player, Game game, Fighter fighter, Enemy enemy, Deck deck) {
+        this.player = player;
+        this.game = game;
+        this.fighter = fighter;
+        this.enemy = enemy;
+        this.deck = deck;
+    }
+    public User getPlayer() {
+        return player;
+    }
+    public void setPlayer(User player) {
+        this.player = player;
+    }
+    public Game getGame() {
+        return game;
+    }
+    public void setGame(Game game) {
+        this.game = game;
+    }
+    public Fighter getFighter() {
+        return fighter;
+    }
+    public void setFighter(Fighter fighter) {
+        this.fighter = fighter;
+    }
+    public Enemy getEnemy() {
+        return enemy;
+    }
+    public void setEnemy(Enemy enemy) {
+        this.enemy = enemy;
+    }
+    public Deck getDeck() {
+        return deck;
+    }
+    public void setDeck(Deck deck) {
+        this.deck = deck;
+    }
+    /*
+        create game + deck
+     */
+}
