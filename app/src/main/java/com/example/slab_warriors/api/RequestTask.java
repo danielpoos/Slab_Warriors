c9662a33032529a952a8a93dbcf67d0c5647b034
Poós Dániel <poosdani@gmail.com>
2022-03-18 10:14:22 +0100

    api

---
+++ app/src/main/java/com/example/slab_warriors/api/RequestTask.java
@@ -0,0 +1,73 @@
+package com.example.slab_warriors.api;
+
+import android.os.AsyncTask;
+import android.util.Log;
+import com.example.slab_warriors.data.User;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.Collection;
+
+public class RequestTask extends AsyncTask<Void, Void, Response> {
+    private String requestType;
+    private String requestParams;
+    private String url;
+    public Response response;
+    public Runnable finalTask;
+    public RequestTask(String url, String requestType) {
+        this.requestType = requestType;
+        this.url = url;
+    }
+    public RequestTask(String url, String requestType, String requestParams) {
+        this.requestType = requestType;
+        this.requestParams = requestParams;
+        this.url = url;
+    }
+    public Runnable getFinalTask() {
+        return finalTask;
+    }
+    public void setFinalTask(Runnable finalTask) {
+        this.finalTask = finalTask;
+    }
+    public RequestTask(){
+        this.finalTask = null;
+    }
+    @Override protected Response doInBackground(Void... voids) {
+        Response response = null;
+        try {
+            switch(requestType){
+                case "get":
+                    response = RequestHandler.get(url); break;
+                case "post":
+                    response = RequestHandler.post(url,requestParams); break;
+                case "put":
+                    response = RequestHandler.put(url,requestParams); break;
+                case "delete":
+                    response = RequestHandler.delete(url+"/"+requestParams); break;
+            }
+            //TODO database address
+        } catch (IOException e) {
+            Log.d("AAAAA", e.toString());
+        }
+        this.response = response;
+        return response;
+    }
+    @Override protected void onPostExecute(Response response) {
+        super.onPostExecute(response);
+        finalTask.run();
+        Gson converter = new Gson();
+        switch(requestType){
+            case "get":
+                User[] users = converter.fromJson(response.getContent(),User[].class);
+                /*userList.clear(); userList.addAll(Arrays.asList(users));*/break;
+            case "post":
+                User newUser = converter.fromJson(response.getContent(),User.class);
+                /*userList.add(newUser);*/break;
+            case "put":
+                User changeUser = converter.fromJson(response.getContent(),User.class);
+                /*userList.add(changeUser);*/break;
+            case "delete":
+                int id = Integer.parseInt(requestParams);
+                /*userList.removeIf(u -> u.getId() == id);*/break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/slab_warriors/api/Response.java
@@ -0,0 +1,16 @@
+package com.example.slab_warriors.api;
+
+public class Response {
+    int responseCode;
+    String content;
+
+    public Response(int responseCode, String content) {
+        this.responseCode = responseCode;
+        this.content = content;
+    }
+
+    public int getResponseCode() { return responseCode; }
+    public void setResponseCode(int responseCode) { this.responseCode = responseCode; }
+    public String getContent() { return content;}
+    public void setContent(String content) { this.content = content; }
+}
