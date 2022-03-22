13af56e1890436e2e21c594a914755b6b67695dd
Poós Dániel <poosdani@gmail.com>
2022-03-22 16:12:55 +0100

    Playfield.java changes

--- app/src/main/java/com/example/slab_warriors/Playfield.java
+++ app/src/main/java/com/example/slab_warriors/Playfield.java
@@ -2,28 +2,14 @@ package com.example.slab_warriors;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 import com.example.slab_warriors.databinding.ActivityPlayfieldBinding;
 
 public class Playfield extends AppCompatActivity {
-    private AppBarConfiguration appBarConfiguration;
     private ActivityPlayfieldBinding binding;
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPlayfieldBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-    }
-    @Override public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp();
     }
 }
