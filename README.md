fc40018431e40091842681c7c07c0bb159f7a16a
Poós Dániel <poosdani@gmail.com>
2022-03-25 23:09:36 +0100

    SelectFragment RecyclerView + Adapter

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Mobile version of our game
 Project update: updated the java, xml and gradle files
 
 Replaced some views with others. Also adjusted the field of the game: now looks like how I wanted 
+
+Created a RecyclerView + Adapter in the fighter selection fragment
--- app/src/main/java/com/example/slab_warriors/fragments/SelectFragment.java
+++ app/src/main/java/com/example/slab_warriors/fragments/SelectFragment.java
@@ -1,28 +1,34 @@
 package com.example.slab_warriors.fragments;
 
-import android.graphics.Color;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.slab_warriors.MenuActivity;
 import com.example.slab_warriors.R;
 import com.example.slab_warriors.data.Fighter;
-import com.example.slab_warriors.databinding.FragmentFighterBinding;
 import com.example.slab_warriors.databinding.FragmentSelectBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class SelectFragment extends Fragment {
+public class SelectFragment extends Fragment{
     public FragmentSelectBinding binding;
-    FragmentFighterBinding bindingFighter;
-    public List<Fighter> fighters;
+    private Handler handler;
+    private FighterAdapter fighterAdapter;
+    private List<Fighter> fighterList;
+    int position = -1;
     @Override public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
         binding = FragmentSelectBinding.inflate(inflater, container, false);
         return binding.getRoot();
@@ -30,32 +36,81 @@ public class SelectFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding.buttonFirst.setOnClickListener(view1 -> NavHostFragment.findNavController(SelectFragment.this)
-                .navigate(R.id.toTheFieldFragment));
+        binding.fighteRView.setHasFixedSize(true);
+        getTestItemsIntoList();
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        binding.fighteRView.setLayoutManager(layoutManager);
+        fighterAdapter = new FighterAdapter(getContext(),fighterList);
+        binding.fighteRView.setAdapter(fighterAdapter);
+        binding.buttonFirst.setOnClickListener(view1 -> {
+                if(position > 0){
+                    NavHostFragment.findNavController(SelectFragment.this).navigate(R.id.toTheFieldFragment);
+                }else {
+                    Toast.makeText(this.getContext(), R.string.no_selected, Toast.LENGTH_SHORT).show();
+                }
+        });
         binding.fab.setOnClickListener(v -> Snackbar.make(v, R.string.choose, Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
-        //Todo listview -> recyclerview
-        //binding.fighterListView.getFocusedChild().setOnClickListener(view2 -> binding.fighterListView.getFocusedChild().setBackgroundColor(Color.CYAN));
-        //binding.fighterListView.setAdapter((ListAdapter) new FighterFragment());
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(keyCode == KeyEvent.KEYCODE_BACK){
+                    Toast.makeText(getContext(), "Valami", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(getActivity(), MenuActivity.class));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
-
+    private void getTestItemsIntoList() {
+        fighterList = new ArrayList<>();
+        fighterList.add(new Fighter("a","a","a",1,2,3));
+        fighterList.add(new Fighter("a","a","a",1,2,3));
+        fighterList.add(new Fighter("a","a","a",1,2,3));
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            public void run(){
+                handler.postDelayed(this, 1000);
+            }
+        }, 1000);
+    }
+    @Override
+    public void onPause() {
+        handler.removeCallbacksAndMessages(null);
+        super.onPause();
+    }
     @Override public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
-
-    private class FighterAdapter extends ArrayAdapter<Fighter>{
-        public FighterAdapter() {
-            super(SelectFragment.this.getContext(),R.layout.fragment_fighter,fighters);
+    private class FighterAdapter extends RecyclerView.Adapter<FighterAdapter.FighterHolder>{
+        public Context context;
+        public List<Fighter> fighters;
+        public FighterAdapter(Context context,List<Fighter> fighters) {
+            this.context = context;
+            this.fighters = fighters;
         }
-        @Override @NonNull public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-            bindingFighter= FragmentFighterBinding.inflate(getLayoutInflater());
-            Fighter colleague = fighters.get(position);
-            bindingFighter.fighterName.setText(colleague.getName());
-            bindingFighter.fighterAttack.setText(colleague.getAttack());
-            bindingFighter.fighterHealth.setText(colleague.getHp());
-            //bindingFighter.fighterImage.setImageBitmap();
-            return bindingFighter.getRoot().getRootView();
+        @NonNull @Override public FighterHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            View view = LayoutInflater.from(context).inflate(R.layout.fragment_fighter,viewGroup,false);
+            return new FighterHolder(view);
+        }
+        @Override public void onBindViewHolder(@NonNull FighterHolder fighterHolder, int i) {
+            position = fighterHolder.getAdapterPosition();
+            fighterHolder.itemView.setOnClickListener(v->Toast.makeText(context, "Position"+i, Toast.LENGTH_SHORT).show());
+        }
+        @Override public int getItemCount() {
+            return fighters.size();
+        }
+        public class FighterHolder extends RecyclerView.ViewHolder {
+            public FighterHolder(@NonNull View itemView) {
+                super(itemView);
+            }
         }
     }
 
